3196bb1ab7d1f01118470959ca6eac087ddedb32
JuliaHoladava <juliaholadava@gmail.com>
2023-05-05 12:56:06 +0200

    feat: add solution for task 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir('./03-files-in-folder/secret-folder', { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+
+  files.forEach(file => {
+    const filePath = path.join(__dirname, 'secret-folder', file.name)
+    fs.stat(filePath,
+    (err, stats) => {
+      if (err) throw err;
+
+      if (stats.isFile()) {
+        console.log(`${path.parse(filePath).name} - ${path.extname(filePath)} - ${stats.size/1000}kb`);
+      }
+    }
+    )
+  })
+})
